cee904947791f67baf510dfd6867aac4d56e4cb3
weyewe <w.yunnal@gmail.com>
2011-10-11 21:15:02 +0700

    ganban starter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ganban::Application.config.secret_token = '94ebfb013a11e58f8002ed6286d87a085aa99dc280ff057995905ec3a6fdf0e981cec71949764bbe940455bb507eca7b918b1e42e2406bcc68be7eb90847ed55'
